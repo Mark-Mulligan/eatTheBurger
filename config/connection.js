f9a6881bb1d212500400accb173f22b64fc3337a
Mark Mulligan <mark.mulligan.jr1@gmail.com>
2021-01-15 18:20:08 -0600

    prepared app for heroku deployment

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,19 @@
 require('dotenv').config();
 const mysql = require("mysql");
+const connection;
 
-const connection = mysql.createConnection({
-    host: process.env.DB_HOST,
-    port: 3306,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: "burgers_db"
-});
+if (process.env.JAWSB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: process.env.DB_HOST,
+        port: 3306,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASS,
+        database: "burgers_db"
+    });
+};
+
+
 
 module.exports = connection;
